c3ce04ee87a06dbc7ec693ffe54e0cfb02b85b1e
Michael Mendoza <michael.mend97@yahoo.com>
2020-05-28 13:56:14 -0400

    Updates

---
+++ precache-manifest.be5a1c42fe2bce961b5247ad811f435c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfd3f7e6c25b8292ec67f65e6cef792",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "2646f68f42f1dbe1de02",
+    "url": "/reactportfolio/static/css/main.3098cf55.chunk.css"
+  },
+  {
+    "revision": "c8058cca1de47c176bd9",
+    "url": "/reactportfolio/static/js/2.d4297b71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactportfolio/static/js/2.d4297b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2646f68f42f1dbe1de02",
+    "url": "/reactportfolio/static/js/main.2bb1ad11.chunk.js"
+  },
+  {
+    "revision": "7fae91689590049c7c7e",
+    "url": "/reactportfolio/static/js/runtime-main.4dfb8aaa.js"
+  },
+  {
+    "revision": "7c5e294caf1e26164f932d94fce2d356",
+    "url": "/reactportfolio/static/media/bp.7c5e294c.png"
+  },
+  {
+    "revision": "d8982a7556211611974dbb7b9e146df8",
+    "url": "/reactportfolio/static/media/grd.d8982a75.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78bb46d6a8fdcea6bdddf65134b215b1.js"
+  "/reactportfolio/precache-manifest.be5a1c42fe2bce961b5247ad811f435c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactportfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
