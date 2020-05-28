f2f9859de9b2f727423c75c4d8b83036bc52c3ea
Michael Mendoza <michael.mend97@yahoo.com>
2020-05-28 16:43:17 -0400

    !

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,12 +14,12 @@ function Main() {
                         <ul>
                             <li><a href='https://github.com/Michael-Mend' target='_blank' rel='noopener noreferrer'>github</a></li>
                             <li><a href='https://www.linkedin.com/in/michael-mendoza-46a61b198/' target='_blank' rel='noopener noreferrer'>linkedIn</a></li>
-                            <li><button>Email</button></li>
+                            <li><a href='/' target='_blank' rel='noopener noreferrer'>Resum&eacute; </a></li>
                         </ul>
                     </div>
                     <div>
                         <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque modi dolores praesentium, aut eius deserunt alias eum, soluta itaque laudantium placeat laborum magnam voluptatum repellendus reiciendis omnis excepturi dolor similique? Doloremque modi dolores praesentium, aut eius deserunt alias eum, soluta itaque laudantium placeat laborum magnam voluptatum repellendus reiciendis omnis excepturi dolor similique?
+                            I'm Mike, a 22 year old full-stack (MERN) web developer based in Orlando, Fl. I am new to programming but I have been using computers for as long as I can remember. Beyond coding; technological literacy, and technology as a whole, are very important to me. Being able to use and undestand technology is going to become an exponentially more relevant skill as time goes on; that being said, I feel like I have stumbled upon my passion with coding and I am excited to grow my knowledge in the years to come.
                         </p>
                     </div>
                 </div>
