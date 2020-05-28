e194860f4a201213abdfa72b6bf705b525a287bc
Michael Mendoza <michael.mend97@yahoo.com>
2020-05-28 13:54:27 -0400

    Updates

---
+++ precache-manifest.78bb46d6a8fdcea6bdddf65134b215b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6104527c3f7cae4655c2c505959850f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "264ef1e6cc883e821c04",
+    "url": "/static/css/main.46d5598f.chunk.css"
+  },
+  {
+    "revision": "c8058cca1de47c176bd9",
+    "url": "/static/js/2.d4297b71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4297b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264ef1e6cc883e821c04",
+    "url": "/static/js/main.2bb1ad11.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "7c5e294caf1e26164f932d94fce2d356",
+    "url": "/static/media/bp.7c5e294c.png"
+  },
+  {
+    "revision": "d8982a7556211611974dbb7b9e146df8",
+    "url": "/static/media/grd.d8982a75.png"
+  }
+]);
