f160a2b7d450b3490de0cae6cd960a7e990719d3
Michael Mendoza <michael.mend97@yahoo.com>
2020-08-25 16:23:47 -0400

    !

--- src/Object.js
+++ src/Object.js
@@ -1,15 +1,21 @@
 export default [{
-    name: 'Zoid',
-    img: 'https://i.imgur.com/sSiikpy.png',
-    link: 'https://projectzoid.herokuapp.com/',
-    git: 'https://github.com/Michael-Mend/ZOID',
-    desc: 'user driven creative forum'
-},{
     name: 'VegHead',
     img: 'https://i.imgur.com/FoFDnkA.png',
     link: 'https://yarocruz.github.io/veg-head/',
     git: 'https://github.com/yarocruz/veg-head',
     desc: 'search engine for vegetarian restaraunts and recipes'
+},{
+    name: 'Budget Tracker',
+    img: 'https://i.imgur.com/30tMWzR.png',
+    link: 'https://mikebudgettracker.herokuapp.com/',
+    git: 'https://github.com/Michael-Mend/budget-tracker',
+    desc: 'visual aid for balancing your budget'
+},{
+    name: 'Fitness Tracker',
+    img: 'https://i.imgur.com/pyT1iGA.png',
+    link: 'https://mikefitnesstracker.herokuapp.com/',
+    git: 'https://github.com/Michael-Mend/fitness-tracker',
+    desc: 'tool for logging your workouts'
 },{
     name: 'Password Generator',
     img: 'https://i.imgur.com/v9D9kkc.png',
